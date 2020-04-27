1a1816293ec875056061d4db7a7136bdb609a588
janDerfler <64102430+janDerfler@users.noreply.github.com>
2020-04-27 22:52:40 +0200

    Added pretty cool button functions

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-$('button').click(() => {
+$('#button1').click(() => {
     console.log('Yeah, you clicked me');
+  })
+
+  $('#button2').click(() => {
+    $('#button1').text('Oh yeah! You did it!');
+  })
+
+  $('#button3').click(() => {
+    var colors = $( "#color" ).val();
+    $('button').css('background', colors);
   })
